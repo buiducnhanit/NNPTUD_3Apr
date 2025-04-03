2d3e0a79588a984fd8aadd39bf0882484119e4d5
ducnhanit <buiducnhan.it@gmail.com>
2025-04-03 14:04:17 +0700

    2180604101 - Bùi Đức Nhân

---
+++ schemas/product.js
@@ -0,0 +1,45 @@
+let mongoose = require('mongoose');
+const slugify = require('slugify');
+
+let productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        unique: true,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true,
+        min:0
+    },description:{
+        type:String,
+        default:""
+    },quantity:{
+        type:Number,
+        default:0,
+        min:0
+    },imgURL:{
+        type:String,
+        default:""
+    },category:{
+        type:mongoose.Types.ObjectId,
+        ref:'category',
+        required:true
+    }
+    ,isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    slug: { type: String, unique: true }
+},{
+    timestamps:true
+})
+
+productSchema.pre('save', function (next) {
+    if (this.isModified('name')) {
+        this.slug = slugify(this.name, { lower: true, strict: true });
+    }
+    next();
+});
+
+module.exports = mongoose.model('product',productSchema);
